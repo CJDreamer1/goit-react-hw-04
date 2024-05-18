1b6a817ef63bafeb4edce03c23e235e29ffebff0
Serhii Vasiliev <sepovi4@gmail.com>
2024-05-18 20:47:38 +0200

    update3

--- src/components/articles-api.js
+++ src/components/articles-api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
 export const getArticles = async (topic, currentPage) => {
-  const response = await axios.get(`/photos`, {
+  const response = await axios.get(`search/photos`, {
     params: {
       client_id: "HckNMPSQDS0G9c12XkQYeNdlDn8SwQhLo79hvgKC6eA",
       query: topic,
@@ -12,5 +12,5 @@ export const getArticles = async (topic, currentPage) => {
     },
   });
   console.log(response);
-  return response.data;
+  return response.data.results;
 };

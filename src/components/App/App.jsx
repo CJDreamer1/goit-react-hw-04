e4db438674a49208ed00636885297ad36090422a
Serhii Vasiliev <sepovi4@gmail.com>
2024-05-20 10:10:56 +0200

    update4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import ImageGallery from "../ImageGallery/ImageGallery";
 import { getArticles } from "../articles-api";
 import SearchBar from "../SearchBar/SearchBar";
 import ImageModal from "../ImageModal/ImageModal";
+import { ProgressBar } from "react-loader-spinner";
+import css from "../App/App.module.css";
 
 export default function App() {
   const [articles, setArticles] = useState([]);
@@ -10,6 +12,7 @@ export default function App() {
   const [isError, setIsError] = useState(false);
   const [page, setPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
   // ================================ Modal State =============================
   const [selectedImage, setSelectedImage] = useState(null);
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -44,7 +47,16 @@ export default function App() {
   };
 
   const handleLoadMore = async () => {
-    setPage(page + 1);
+    setIsLoadingMore(true);
+    setPage((prevPage) => prevPage + 1);
+    try {
+      const data = await getArticles(searchQuery, page + 1);
+      setArticles((prevState) => [...prevState, ...data]);
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsLoadingMore(false);
+    }
   };
   // ========================================= Modal Window ====================
   const openModal = (image) => {
@@ -57,18 +69,34 @@ export default function App() {
     setSelectedImage(null);
   };
   // =================================== End Modal window ======================
+  const bar = (
+    <ProgressBar
+      visible={true}
+      height="80"
+      width="500"
+      color="#0056b3"
+      borderColor="#0056b3"
+      ariaLabel="progress-bar-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      className={css.progressBar}
+    />
+  );
+
   return (
     <div>
-      <h1>HTTP request in React</h1>
       <SearchBar onSearch={handleSearch} />
-      {isLoading && <p>Loading articles, please wait...</p>}
+      {isLoading && <div className={css.progressWrapper}>{bar}</div>}
       {isError && <p>Ooops! There was an error, try reloading page!</p>}
       {articles.length > 0 && (
         <ImageGallery items={articles} onImageClick={openModal} />
       )}
       {articles.length > 0 && !isLoading && (
-        <button onClick={handleLoadMore}>Load more</button>
+        <button className={css.loadMoreBtn} onClick={handleLoadMore}>
+          Load more
+        </button>
       )}
+      {isLoadingMore && <div className={css.progressWrapper}>{bar}</div>}
       <ImageModal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,12 +2,12 @@ import css from "../ImageCard/ImageCard.module.css";
 
 export default function ImageCard({ item }) {
   return (
-    <div>
+    <>
       <img
         className={css.image}
         src={item.urls.small}
         alt={item.alt_description}
       />
-    </div>
+    </>
   );
 }
